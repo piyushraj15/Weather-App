f11e86fb834fcba58387d3892324025f43ca71e3
piyushraj15 <piyush.raj73@gmail.com>
2020-03-31 00:21:33 +0530

    check

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+const api={
+  key:"c7f308fef68d30d8c65cd151b1120348",
+  base:"https://samples.openweathermap.org/data/2.5/"
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <main>
+          <input className="search" placeholder="Search" type="text"></input>
+        </main>
       </div>
     );
   }
