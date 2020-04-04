67d90600c0c155020e66fe4b060e067cd5f7e162
piyushraj15 <piyush.raj73@gmail.com>
2020-04-04 10:58:10 +0530

    to change background according to the temperature

--- src/App.js
+++ src/App.js
@@ -42,7 +42,7 @@ function App() {
   } 
   
     return (
-      <div className="App">
+      <div className={(typeof weather.main != "undefined") ? ((weather.main.temp > 16) ? 'App warm' : 'App') : 'App'}>
         <main>
           <div className="search-box">
           <input className="search-bar" 
